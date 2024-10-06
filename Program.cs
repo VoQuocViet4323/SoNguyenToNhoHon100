b4e76e83143c4cba02716a57835ff6310161edad
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-06 23:07:12 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int dem = 0;
+        Console.WriteLine("Danh sach so nguyen to nho hon 100:");
+        for (int i = 2; i <= 100; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    dem++;
+                }
+            }
+
+            if (dem == 2)
+            {
+                Console.WriteLine(i);
+            }
+            dem = 0;
+        }
+    }
+}
